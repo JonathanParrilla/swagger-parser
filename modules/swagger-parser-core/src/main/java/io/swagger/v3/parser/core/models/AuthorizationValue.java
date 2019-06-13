05af9c272a74337d25456cfc7d6b44d6feb1804e
Jonathan Parrilla <jonathan.parrilla@smartbear.com>
2019-06-13 16:00:16 -0400

    Update AuthorizationValue.java

--- modules/swagger-parser-core/src/main/java/io/swagger/v3/parser/core/models/AuthorizationValue.java
+++ modules/swagger-parser-core/src/main/java/io/swagger/v3/parser/core/models/AuthorizationValue.java
@@ -1,58 +1,77 @@
 package io.swagger.v3.parser.core.models;
 
-public class AuthorizationValue {
+// Overloaded Constructor named AuthorizationValue
+public class AuthorizationValue 
+{
     private String value, type, keyName;
-
-    public AuthorizationValue() {
+    
+    // Accepted variances of AuthorizationValue
+    public AuthorizationValue() 
+    {
     }
 
-    public AuthorizationValue(String keyName, String value, String type) {
+    public AuthorizationValue(String keyName, String value, String type) 
+    {
         this.setKeyName(keyName);
         this.setValue(value);
         this.setType(type);
     }
 
-    public AuthorizationValue value(String value) {
+    public AuthorizationValue value(String value) 
+    {
         this.value = value;
         return this;
     }
 
-    public AuthorizationValue type(String type) {
+    public AuthorizationValue type(String type) 
+    {
         this.type = type;
         return this;
     }
 
-    public AuthorizationValue keyName(String keyName) {
+    public AuthorizationValue keyName(String keyName) 
+    {
         this.keyName = keyName;
         return this;
     }
 
-    public String getValue() {
+    // Getters and Setters for Value
+    public String getValue() 
+    {
         return value;
     }
 
-    public void setValue(String value) {
+    public void setValue(String value) 
+    {
         this.value = value;
     }
 
-    public String getType() {
+    // Getters and Setters for Type
+    public String getType() 
+    {
         return type;
     }
 
-    public void setType(String type) {
+    public void setType(String type) 
+    {
         this.type = type;
     }
 
-    public String getKeyName() {
+    // Getters and Setters for KeyName
+    public String getKeyName() 
+    {
         return keyName;
     }
 
-    public void setKeyName(String keyName) {
+    public void setKeyName(String keyName) 
+    {
         this.keyName = keyName;
     }
 
+    // Some overrides
     @Override
-    public int hashCode() {
+    public int hashCode() 
+    {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((keyName == null) ? 0 : keyName.hashCode());
@@ -62,36 +81,53 @@ public class AuthorizationValue {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
+    public boolean equals(Object obj) 
+    {
+        if (this == obj) 
+        {
             return true;
         }
-        if (obj == null) {
+        if (obj == null) 
+        {
             return false;
         }
-        if (getClass() != obj.getClass()) {
+        if (getClass() != obj.getClass()) 
+        {
             return false;
         }
+        
         AuthorizationValue other = (AuthorizationValue) obj;
-        if (keyName == null) {
-            if (other.keyName != null) {
+        if (keyName == null) 
+        {
+            if (other.keyName != null) 
+            {
                 return false;
             }
-        } else if (!keyName.equals(other.keyName)) {
+        } 
+        else if (!keyName.equals(other.keyName)) 
+        {
             return false;
         }
-        if (type == null) {
-            if (other.type != null) {
+        if (type == null) 
+        {
+            if (other.type != null) 
+            {
                 return false;
             }
-        } else if (!type.equals(other.type)) {
+        } 
+        else if (!type.equals(other.type)) 
+        {
             return false;
         }
-        if (value == null) {
-            if (other.value != null) {
+        if (value == null) 
+        {
+            if (other.value != null) 
+            {
                 return false;
             }
-        } else if (!value.equals(other.value)) {
+        } 
+        else if (!value.equals(other.value)) 
+        {
             return false;
         }
         return true;
